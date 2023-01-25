886197e71690d49da8d2a269711d9bd068cd682b
kurtyjlee <kurtyjlee@gmail.com>
2023-01-25 15:21:26 +0800

    changed authentication

--- backend/routes/routes.go
+++ backend/routes/routes.go
@@ -11,6 +11,21 @@ func Setup(app *fiber.App) {
 	app.Post("/api/register", controllers.Register)
 	app.Post("/api/login", controllers.Login)
 
+	app.Get("/api/user", controllers.User)
+	app.Get("/api/users/:id", controllers.GetUser)
+
+	app.Get("/api/roles", controllers.AllRoles)
+
+	app.Get("/api/posts", controllers.AllPosts)
+	app.Get("/api/posts/:id", controllers.GetPost)
+	app.Static("/api/uploads/image", "./uploads") // Serve static files
+
+	app.Get("/api/comments", controllers.AllComments)
+	app.Get("/api/comments/:id", controllers.GetComment)
+
+	app.Get("/api/threads", controllers.AllThreads)
+	app.Get("/api/threads/:id", controllers.GetThread)
+
 	// Make sure that the user is authenticated for the pages below
 	app.Use(middleware.IsAuthenticated)
 
@@ -18,16 +33,13 @@ func Setup(app *fiber.App) {
 	app.Put("/api/user/info", controllers.UpdateInfo)
 	app.Put("/api/user/password", controllers.UpdatePassword)
 
-	app.Get("/api/user", controllers.User)
 	app.Post("/api/logout", controllers.Logout)
 
 	app.Get("/api/users", controllers.AllUsers)
 	app.Post("/api/users", controllers.CreateUser)
-	app.Get("/api/users/:id", controllers.GetUser)
 	app.Put("/api/users/:id", controllers.UpdateUser)
 	app.Delete("/api/users/:id", controllers.DeleteUser)
 
-	app.Get("/api/roles", controllers.AllRoles)
 	app.Post("/api/roles", controllers.CreateRole)
 	app.Get("/api/roles/:id", controllers.GetRole)
 	app.Put("/api/roles/:id", controllers.UpdateRole)
@@ -35,25 +47,18 @@ func Setup(app *fiber.App) {
 
 	app.Get("/api/permissions", controllers.AllPermissions)
 
-	app.Get("/api/posts", controllers.AllPosts)
 	app.Post("/api/posts", controllers.CreatePost)
-	app.Get("/api/posts/:id", controllers.GetPost)
 	app.Put("/api/posts/:id", controllers.UpdatePost)
 	app.Delete("/api/posts/:id", controllers.DeletePost)
 	app.Get("/api/posts/threads/:id", controllers.FilterPost)
 
 	app.Post("/api/uploads/image", controllers.UploadImage)
-	app.Static("/api/uploads/image", "./uploads") // Serve static files
 
-	app.Get("/api/comments", controllers.AllComments)
 	app.Post("/api/comments", controllers.CreateComment)
-	app.Get("/api/comments/:id", controllers.GetComment)
 	app.Put("/api/comments/:id", controllers.UpdateComment)
 	app.Delete("/api/comments/:id", controllers.DeleteComment)
 
-	app.Get("/api/threads", controllers.AllThreads)
 	app.Post("/api/threads", controllers.CreateThread)
-	app.Get("/api/threads/:id", controllers.GetThread)
 	app.Put("/api/threads/:id", controllers.UpdateThread)
 	app.Delete("/api/threads/:id", controllers.DeleteThread)
 }
--- frontend/src/pages/posts/Posts.tsx
+++ frontend/src/pages/posts/Posts.tsx
@@ -9,26 +9,12 @@ import { Paginator } from "../../components/Paginator";
 import { useParams } from "react-router-dom";
 import { PostContainer } from "../../components/PostContainer";
 
-// For animation
-const hide = {
-  maxHeight: 0,
-  transition: '200ms ease-in'
-}
-
-const show = {
-  maxHeight: '150px',
-  transition: '200ms ease-out'
-}
-
 export const Posts = () => {
   const [posts, setPosts] = useState([]);
 
   // Pagination
   const [page, setPage] = useState(1);
   const [lastPage, setLastPage] = useState(0);
-
-  // Animation
-  const [selected, setSelected] = useState(0);
 
   // thread id
   let id: any = useParams();
@@ -51,18 +37,6 @@ export const Posts = () => {
     )()
   }, [page, threadId]);
 
-  const select = (id: number) => {
-    setSelected(selected === id ? 0 : id);
-  }
-
-  const del = async (id:number) => {
-    if (window.confirm("Are you sure you want to delete?")) {
-      await axios.delete(`posts/${id}`);
-
-      setPosts(posts.filter((p: Post) => p.id !== id));
-    }
-  }
-
   return (
     <Wrapper>
       <div className="main-post-container"> 
@@ -80,85 +54,3 @@ export const Posts = () => {
     </Wrapper>
   )
 }
-
-// {/* <div className="table-responsive">
-//         <table className="table text-white table-sm">
-//           <thead>
-//             <tr>
-//               <th>#</th>
-//               <th>Image</th>
-//               <th>Description</th>
-//               <th>Title</th>
-//               <th>Likes</th>
-//               <th>thread_id</th>
-//               <th>Action</th>
-//             </tr>
-//           </thead>
-//           <tbody>
-//             {posts.map((post: Post) => {
-//               return (
-//                 <>
-//                   <tr key={post.id}>
-//                     <td>{post.id}</td>
-//                     <td><img src={post.image} width="50"/></td>
-//                     <td>{post.title}</td>
-//                     <td>{post.description}</td>
-//                     <td>{post.total_likes}</td>
-//                     <td>{post.thread_id}</td>
-//                     {/* Actions */}
-//                     <td>
-//                       <div className="btn-group mr-3">
-//                         <Link 
-//                           className="btn btn-sm btn-outline-secondary text-white"
-//                           to={`/posts/${post.id}/edit`}
-//                         >Edit</Link>
-//                         <a 
-//                           className="btn btn-sm btn-outline-secondary text-white"
-//                           href="#!"
-//                           onClick={() => del(post.id)}
-//                         >Delete</a>
-//                         <a 
-//                           className="btn btn-sm btn-outline-secondary text-white"
-//                           href="#!"
-//                           onClick={() => select(post.id)}
-//                         >View</a>
-//                         <Link 
-//                         className="btn btn-sm btn-outline-secondary text-white"
-//                           to={`/posts/${post.id}/comment_create`}
-//                         >Comment</Link>
-//                       </div>
-//                     </td>
-//                   </tr>
-//                   {/* comments */}
-//                   <tr>
-//                     <td colSpan={10}>
-//                       <div className="overflow-hidden" style={selected === post.id ? show : hide}>
-//                         <table className="table table-sm text-white">
-//                           <thead>
-//                             <tr>
-//                               <th>#</th>
-//                               <th>description</th>
-//                               <th>likes</th>
-//                             </tr>
-//                           </thead>
-//                           <tbody>
-//                           {post.comments.map((comment: Comments) => {
-//                             return (
-//                               <tr>
-//                                 <td>{comment.id}</td>
-//                                 <td>{comment.description}</td>
-//                                 <td>{comment.total_likes}</td>
-//                               </tr>
-//                             )
-//                           })}
-//                           </tbody>
-//                         </table>
-//                       </div>
-//                     </td>
-//                   </tr>
-//                 </>
-//               )
-//             })}
-//           </tbody>
-//         </table>
-//       </div> */}
